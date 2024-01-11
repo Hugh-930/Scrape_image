401f406a21297edc8e0e29be8e8b4602caa2f398
hugh <yasudamura@gmail.com>
2024-01-11 10:45:03 +0900

    refactor: import remaining Selenium support modules

--- collect_links.py
+++ collect_links.py
@@ -18,6 +18,13 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import ElementNotVisibleException, StaleElementReferenceException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 class CollectLinks:
     def __init__(self, no_gui=False):
