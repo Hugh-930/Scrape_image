98b5ca06a228b8883929782d914f6dd7fed2a0f7
hugh <yasudamura@gmail.com>
2024-01-19 14:19:15 +0900

    feat: add main entrypoint with Sites enum

---
+++ main.py
@@ -0,0 +1,44 @@
+"""
+Copyright 2018 YoongiKim
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+import os
+import requests
+import shutil
+from multiprocessing import Pool
+import signal
+import argparse
+from collect_links import CollectLinks
+import imghdr
+
+
+class Sites:
+    GOOGLE = 1
+    NAVER = 2
+    GOOGLE_FULL = 3
+    NAVER_FULL = 4
+
+    @staticmethod
+    def get_text(code):
+        if code == Sites.GOOGLE:
+            return 'google'
+        elif code == Sites.NAVER:
+            return 'naver'
+        elif code == Sites.GOOGLE_FULL:
+            return 'google'
+        elif code == Sites.NAVER_FULL:
+            return 'naver'
+
+    @staticmethod
